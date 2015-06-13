1af1877c5a87d5eafdfa1acfa5a6e3a9bdc672d4
Derrick Liang <Derrickl920@gmail.com>
2015-06-12 17:47:37 -0700

    added GoT style

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ session_start();
  	<title>THE WALL LOGIN PAGE</title>
  	<meta charset='utf-8'>
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
  	<link rel="stylesheet" type="text/css" href="styles.css">
  </head>
  <body>
@@ -26,27 +25,29 @@ session_start();
  		unset($_SESSION['success_message']);
  	}
  	 ?>
- <div id="container">
- 	 <div id="register">
-	 	<h1>Register</h1>
-	  	<form action="process.php" method="post">
-	  		First Name: <input type="text" name="first_name"><br>
-	  		Last Name: <input type="text" name="last_name"><br>
-	 		Email Address: <input type="text" name="email"><br>
-	 		Password: <input type="password" name="password"><br>
-	 		Confirm Password: <input type="password" name="confirm_password"><br>
-	 		<input type="submit" name="register" value="Register">
-	 		<input type="hidden" name="action" value="register">
-	 	</form>
- 	</div>
- 	<div id="login">
- 	<h1>Login</h1>
- 	<form action="process.php" method="post">
- 		Email Address: <input type="text" name="email"><br>
- 		Password: <input type="password" name="password"><br>
- 		<input type="submit" value="Login">
- 		<input type="hidden" name="action" value="login">
- 	</form>
+ <div id="home-container">
+ 	 <div id="login-register">
+	 	<h1 class='text-primary'>Register</h1>
+	 	<div class="form-group">
+		  	<form action="process.php" method="post">
+		  		<input class='form-control col-xs-4' type="text" name="first_name" placeholder='First Name'><br>
+		  		<input class='form-control col-xs-4' type="text" name="last_name" placeholder='Last Name'><br>
+		 		<input class='form-control col-xs-4' type="text" name="email" placeholder="Email"><br>
+		 		<input class='form-control col-xs-4' type="password" name="password" placeholder="Password"><br>
+		 		<input class='form-control col-xs-4' type="password" name="confirm_password" placeholder="Confirm Password"><br>
+		 		<input class='btn btn-primary' type="submit" name="register" value="Register">
+		 		<input type="hidden" name="action" value="register">
+		 	</form>
+	 	</div>
+	 	<h1 class='text-success'>Login</h1>
+	 	<div class="form-group">
+		 	<form action="process.php" method="post">
+		 		<input class='form-control col-xs-4' type="text" name="email" placeholder='Email'><br>
+		 		<input class='form-control col-xs-4' type="password" name="password" placeholder='Password'><br>
+		 		<input  class='btn btn-success' type="submit" value="Login">
+		 		<input type="hidden" name="action" value="login">
+		 	</form>
+ 		</div>
 	</div>
 </div>
  </body>
--- thewall.php
+++ thewall.php
@@ -8,23 +8,29 @@ require('new-connection.php');
  <head>
  	<title>THE WALL</title>
  	<meta charset='utf-8'>
+ 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="styles.css">
  </head>
  <body>
- 	<div id="container">
+ 	<div id="wall-container">
  		<div id="header">
- 			<h2>Coding Dojo Wall</h2>
- 			<h4> Welcome <?="{$_SESSION['first_name']}"?></h4>
- 			<a href='process.php'>LOG OFF</a>
+ 			<div class="row">
+ 				<h1 class='col-xs-8'>The Wall - Game Of Thrones</h1>
+ 				<h3 class='col-xs-2'> Welcome <span style='text-transform: capitalize;'><?="{$_SESSION['first_name']}!"?></span></h3>
+ 				<button onclick="window.location='process.php';" type="button" class='btn btn-danger col-xs-2'>LOG OFF</button>
+			</div>
 			<hr>
  		</div>
  		<div id="content">
- 			<h3>Post a message</h3><br>
- 			<form action ="wallprocess.php" method="post">
- 				<input type="textarea" name="entermessage" id="textbox" ><br>
- 				<input type="submit" value="Post a message">
- 				<input type="hidden" name="action" value="message">
- 			</form>
+ 			<div id='postamessage'>
+	 			<h2>Post a message to The Wall</h2>
+	 			<form action ="wallprocess.php" method="post">
+	 				<textarea name="entermessage" id="textbox" ></textarea><br>
+	 				<input class='btn btn-primary btn-sm' type="submit" value="Post a message">
+	 				<input type="hidden" name="action" value="message">
+	 			</form>
+	 			<hr>
+ 			</div>
  			<div id="wallpost">
 <?php
 	$query3 = "SELECT messages.message, users.first_name, users.last_name, messages.created_at, messages.updated_at, messages.id 
@@ -32,36 +38,44 @@ require('new-connection.php');
 				LEFT JOIN users ON users.id = messages.users_id
 				ORDER BY messages.created_at DESC";
 	$messagepost = fetch_all($query3);
-		?>	<div id='messagebox'>
-<?php		foreach($messagepost as $message)
-			{	
-				$date = date('F dS, Y', strtotime($message['created_at']));
-	?>			<div id='message'>
-<?php				echo "<h3>{$message['first_name']} {$message['last_name']}  {$date}</h3> <br>";
-					echo $message['message']. '<br><br>'; ?>
-						<p>Post a comment</p><br>
- 							<form  id="commentform" action ="wallprocess.php" method="post">
-				 				<input type="text" name="entercomment" id="commentbox" ><br>
-				 				<input type = "hidden" name="comment_id" value="<?= $message['id']?>">
-				 				<input type="submit" value="Post a comment">
-				 				<input type="hidden" name="action" value="reply">
-				 			</form>
-<?php  	$query4 = "SELECT comments.comment, users.first_name, users.last_name, messages.id, comments.users_id, comments.created_at, comments.created_at
- 					FROM comments
- 					LEFT JOIN messages ON messages.id = comments.messages_id
- 					LEFT JOIN users ON users.id = comments.users_id
- 					WHERE messages_id = $message[id]
- 					ORDER BY comments.created_at ASC";
-					$commentpost = fetch_all($query4);
-					foreach($commentpost as $comment)
-					{
-						echo "<h5>{$comment['first_name']} {$comment['last_name']} {$date} </h5>";
-						echo $comment['comment'].'<br>';
-					} ?>
-
+			?>	<div id='messagebox'>
+		<?php		foreach($messagepost as $message)
+					{	
+						$date = date('F dS, Y g:iA', strtotime($message['created_at']));
+			?>			<div id='message'>
+		<?php				echo "<h3> <span style='text-transform: capitalize'>{$message['first_name']}</span> <span style='text-transform: capitalize'> {$message['last_name']}</span> - {$date}</h3> <br>";
+							?>
+							<div id='postedmessage'>
+		<?php				echo "<p>{$message['message']}</p>". '<br>'; ?>
+							</div>
+		<?php  	$query4 = "SELECT comments.comment, users.first_name, users.last_name, messages.id, comments.users_id, comments.created_at, comments.created_at
+		 					FROM comments
+		 					LEFT JOIN messages ON messages.id = comments.messages_id
+		 					LEFT JOIN users ON users.id = comments.users_id
+		 					WHERE messages_id = $message[id]
+		 					ORDER BY comments.created_at ASC";
+							$commentpost = fetch_all($query4);
+							foreach($commentpost as $comment)
+							{	?>
+						<div id='newcomment'>
+							<div id='commentname'>
+		<?php					echo "<h4><span style='text-transform: capitalize'>{$comment['first_name']}</span><span style='text-transform: capitalize'> {$comment['last_name']}</span> - {$date}  </h4>";	?>
+							</div>
+							<div id='postedcomment'>				
+		<?php					echo "<p>{$comment['comment']}</p>".'<br>';
+							} ?>
+							</div>
+		 						<form  id="commentform" action ="wallprocess.php" method="post">
+									<textarea name="entercomment" id="commentbox"></textarea><br>
+					 				<input type = "hidden" name="comment_id" value="<?= $message['id']?>">
+					 				<input class='btn btn-success btn-sm' type="submit" value="Post a comment">
+							 		<input type="hidden" name="action" value="reply">
+						 		</form>
+						 </div>
+						</div>
+						<hr>
+<?php		} ?>
 				</div>
-<?php		} ?>			
-			</div>
  			</div>
  		</div>
  	</div>
